ea9a3aaaae8a2f1095569b59789960ab07db88d7
numan.hassan <numan.hassan@purelogics.net>
2015-02-05 18:00:42 +0500

    employer dashboard status

--- application/views/employer/dashboard/status_tab.php
+++ application/views/employer/dashboard/status_tab.php
@@ -29,7 +29,7 @@
             if($jobs){
             foreach ($jobs as $key=>$row) { ?>
 
-                <div class="status-list-item">
+                <div class="status-list-item" id="job-applied-list-item_<?php echo $row['job_applied_id']; ?>" >
                     
                     <div class="col col-sm-2 ">
                         <div style="float: left; width: 150px; vertical-align: top; font-size: 12px; padding-left: 5px; padding-top: 5px;" class="">
@@ -43,27 +43,31 @@
                     <div class="col col-sm-7">
                         <div style="float: left; padding-top: 14px;">
                             <?php
-                            $matched_class = ($row['matched'] == 1 ) ? " btn-success " : "btn-danger" ;
-                            $interview_class = ($row['interview'] == 1 ) ? " btn-success " : "btn-danger" ;
-                            $interview_complete_class = ($row['interview_complete'] == 1 ) ? " btn-success " : "btn-danger" ;
-                            $face_2_face_class = ($row['face_2_face'] == 1 ) ? " btn-success " : "btn-danger" ;
-                            $job_offer_class = ($row['job_offer'] == 1 ) ? " btn-success " : "btn-danger" ;
+                            $matched_class = ($row['matched'] == 1 ) ? " btn-success " : "btn-danger update_job_status" ;
+                            $interview_class = ($row['interview'] == 1 ) ? " btn-success " : "btn-danger update_job_status" ;
+                            $interview_complete_class = ($row['interview_complete'] == 1 ) ? " btn-success " : "btn-danger update_job_status" ;
+                            $face_2_face_class = ($row['face_2_face'] == 1 ) ? " btn-success " : "btn-danger update_job_status" ;
+                            $job_offer_class = ($row['job_offer'] == 1 ) ? " btn-success " : "btn-danger update_job_status" ;
 
                             ?>
-                            <span class="btn btn-sm btn-success job_status_btn">Applied</span>
-                            <span  class="btn btn-sm <?php echo $matched_class; ?> job_status_btn">Matched</span>
-                            <span class="btn btn-sm <?php echo $interview_class; ?> job_status_btn">Interview</span>
-                            <span class="btn btn-sm <?php echo $interview_complete_class; ?> job_status_btn">Interview Complete</span>
-                            <span class="btn btn-sm <?php echo $face_2_face_class; ?> job_status_btn">Face 2 Face</span>
-                            <span  class="btn btn-sm <?php echo $job_offer_class; ?> job_status_btn">Job Offer</span>
+                            <span class="btn btn-sm btn-success">Applied</span>
+                                    <span  class="btn btn-sm <?php echo $matched_class; ?> " data-type="matched" data-id="<?php echo $row['job_applied_id']; ?>" >Matched</span>
+                                    <span class="btn btn-sm <?php echo $interview_class; ?> " data-type="interview" data-id="<?php echo $row['job_applied_id']; ?>">Interview</span>
+                                    <span class="btn btn-sm <?php echo $interview_complete_class; ?> " data-type="interview_complete" data-id="<?php echo $row['job_applied_id']; ?>" >Interview Complete</span>
+                                    <span id="job_applied_Face2Face_<?php echo $row['job_applied_id']; ?>" class="btn btn-sm <?php echo $face_2_face_class; ?> " data-type="face_2_face" data-id="<?php echo $row['job_applied_id']; ?>">Face 2 Face</span>
+                                    <span id="job_applied_jobOffer_<?php echo $row['job_applied_id']; ?>" class="btn btn-sm <?php echo $job_offer_class; ?> " data-type="job_offer" data-id="<?php echo $row['job_applied_id']; ?>">Job Offer</span>
+                                    
                         </div>
                     </div>
                      
                     <div class="col col-sm-1 text-right">
                          
-                        <a href="javascript:void(0)">
-                            <span class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-trash"></span></span>
+                        <a href="javascript:void(0)" class="delete_job_applied" data-value="<?php echo $row['job_applied_id']; ?>">
+                            <span class="btn btn-xs btn-danger">
+                                <span class="glyphicon glyphicon-trash"></span>
+                            </span>
                         </a>
+                        
                     </div>
                     <div class="clearfix"></div>
                 </div>
