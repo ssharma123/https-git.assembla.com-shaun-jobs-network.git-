2b4c9e9be8cdfdc7a1b7c6ec407f93f7be59c070
numan.hassan <numan.hassan@purelogics.net>
2015-03-04 17:22:35 +0500

    sajari API

--- application/views/layouts/employer.php
+++ application/views/layouts/employer.php
@@ -211,6 +211,20 @@
             <?php echo load_js("additional-methods.js",'assets/js/jquery-validation/'); ?>
 <!--            Validation Plugin END -->
             <?php echo load_js("employee.js"); ?>
+
+<script type="text/javascript">
+    var _sj = _sj || [];
+    _sj.push(['company', 'medmatch']);
+    _sj.push(['collection', 'medmatchjobs']);
+    (function () {
+        var sj = document.createElement('script');
+        sj.type = 'text/javascript';
+        sj.async = true;
+        sj.src = 'https://www.sajari.com/js/sj.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(sj, s);
+    })();
+ </script>
             
         </div>
     </body>
