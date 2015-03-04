3cf147588e09122c05b60ae7d1f761700f48fbbd
numan.hassan <numan.hassan@purelogics.net>
2015-03-04 17:17:26 +0500

    sajari API

--- sajari_add.php
+++ sajari_add.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require 'vendor/autoload.php';
+require 'sajari/vendor/autoload.php';
 
 use Sajari\Engine\EngineClient;
 use Sajari\Engine\Exception\EngineException;
