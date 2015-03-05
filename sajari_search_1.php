877ced1f884952e71bc2cbe0ec160a09dcf3e859
numan.hassan <numan.hassan@purelogics.net>
2015-03-05 14:32:42 +0500

    ingnore file

--- sajari_search_1.php
+++ sajari_search_1.php
@@ -25,7 +25,7 @@ try {
         "filters"=>array(
             array("~title" => "developer"),
             array("specialty" => "1"),
-//            array("subspecialty" => "developer"),
+            array("subspecialty" => "5"),
         ),
         'maxresults' => 10,
     ));
