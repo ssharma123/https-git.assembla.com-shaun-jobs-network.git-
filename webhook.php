f3c3c379f3b685544753c1b79e2f712654a01fb3
numan.hassan <numan.hassan@purelogics.net>
2015-03-07 18:47:46 +0500

    Sajari API

--- webhook.php
+++ webhook.php
@@ -4,14 +4,14 @@ ini_set('display_errors', 1);
 
 $data = @file_get_contents("php://input");
 
-$file = fopen("uploads/data_1.txt", "w");
+$file = fopen("uploads/data_1.txt", "a");
 fwrite($file, $data);
 fwrite($file, "\n");
 fclose($file);
 
 $data = $_POST;
 
-$file = fopen("uploads/data_2.txt", "w");
+$file = fopen("uploads/data_2.txt", "a");
 fwrite($file, $data);
 fwrite($file, "\n");
 fclose($file);
