3529d6f7b7b534f14dff4f4638363776c63e1736
numan.hassan <numan.hassan@purelogics.net>
2015-03-07 20:50:43 +0500

    Sajari API

--- application/views/job_seeker/match.php
+++ application/views/job_seeker/match.php
@@ -36,8 +36,6 @@
             
             <?php
             
-            error_reporting(E_ALL);
-            ini_set('display_errors', 1);
             $jobs_sorted_array = array();
             if($jobs_data){
                 
