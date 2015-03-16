01f11d2c218f20145bea784d144837367679651f
numan.hassan <numan.hassan@purelogics.net>
2015-03-16 13:19:33 +0500

    Sajari API

--- sajari_search_2.php
+++ sajari_search_2.php
@@ -12,10 +12,9 @@ $sajari = new Sajari();
 $params['meta']['specialty'] = '136';
 $params['meta']['sub_specialty'] = '137';
 
-//$params['meta']['lat'] = '42.7175688';
-//$params['meta']['lng'] = '-77.0435795';
+$params['meta']['lat'] = '42.7175688';
+$params['meta']['lng'] = '-77.0435795';
 
-$params['scales'] = 'lat,42.7175688,100,5,0|lng,-77.0435795,100,5,0';
 $params['maxresults'] = 40;
 
 $rsp = $sajari->sajari_search($params);
