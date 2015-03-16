d9fda44af005d42e24172de0af1f03088fe85448
numan.hassan <numan.hassan@purelogics.net>
2015-03-16 15:11:13 +0500

    Sajari API

--- sajari_search_2.php
+++ sajari_search_2.php
@@ -12,9 +12,15 @@ $sajari = new Sajari();
 $params['meta']['specialty'] = '136';
 $params['meta']['sub_specialty'] = '137';
 
-$params['meta']['lat'] = '51.55';
-$params['meta']['lng'] = '100.667';
+$params['meta']['lat'] = '42.7175688';
+$params['meta']['lng'] = '-77.0435795';
 
+$params['meta']['salary_range_max'] = '35000';
+$params['meta']['salary_range_min'] = '50000';
+
+$params['meta']['department_size'] = '20-40';
+
+$params['scales'] = 'salary_range_min,35000,26000,1,0|salary_range_max,50000,26000,1,0';
 
 
 $params['maxresults'] = 20;
