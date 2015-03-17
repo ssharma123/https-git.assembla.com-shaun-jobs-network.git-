0fc505e0c294299a4a9043d23220853f737741f9
numan.hassan <numan.hassan@purelogics.net>
2015-03-17 17:50:34 +0500

    Sajari API

--- application/libraries/google/google_geolocation.php
+++ application/libraries/google/google_geolocation.php
@@ -28,7 +28,7 @@ class Google_geolocation {
         
         $url = $this->geocoding.$urlParam . "sensor=" . $this->sensor;
         
-
+        echo $url; die;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
 
