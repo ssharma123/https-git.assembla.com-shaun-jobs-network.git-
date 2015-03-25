b7ab94f4ce196582ba2986c1a6467952c1681094
numan.hassan <numan.hassan@purelogics.net>
2015-03-25 18:09:30 +0500

    resume pharsing

--- application/controllers/employee_dashboard.php
+++ application/controllers/employee_dashboard.php
@@ -730,6 +730,13 @@ class Employee_dashboard extends MY_EmployerController {
                 }
             }
             
+            $numeric_vals = array("salary_range_min","salary_range_max","employer_id");
+            foreach($job as $k=>$v){
+                if( in_array($k, $numeric_vals) ){
+                    $job[$k] = (int) $v;
+                }
+            }
+            
             if($job['sajari_doc_id'] == ""){
                 // ADD to sajari
                 $params = array(
